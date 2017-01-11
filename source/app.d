ef69b40844e7431cef0ca24154c0ee2ef089d156
Michael Fadely <michaelwfadely@gmail.com>
2017-01-10 21:12:41 -0700

    wow this works

--- source/app.d
+++ source/app.d
@@ -3,14 +3,51 @@ import vibe.d;
 shared static this()
 {
 	auto settings = new HTTPServerSettings;
-	settings.port = 8080;
+	settings.port = 3128;
 	settings.bindAddresses = ["::1", "127.0.0.1"];
-	listenHTTP(settings, &hello);
+	listenHTTP(settings, &passThrough);
 
-	logInfo("Please open http://127.0.0.1:8080/ in your browser.");
+	logInfo("Proxy started on port 3128.");
 }
 
-void hello(HTTPServerRequest req, HTTPServerResponse res)
+void passThrough(HTTPServerRequest req, HTTPServerResponse res)
 {
-	res.writeBody("Hello, World!");
+	logInfo(req.toString());
+
+	// TODO: CONNECT method
+
+	requestHTTP(req.requestURL,
+		(scope HTTPClientRequest r)
+		{
+			r.headers = req.headers;
+			r.method = req.method;
+			r.bodyWriter.write(req.bodyReader);
+			logInfo(r.toString());
+		},		
+		(scope HTTPClientResponse r)
+		{
+			auto status = cast(HTTPStatus)r.statusCode;
+
+			if (isSuccessCode(status))
+			{
+				res.headers = r.headers;
+				res.bodyWriter.write(r.bodyReader);
+			}
+			else
+			{
+				with (HTTPStatus) switch (status)
+				{
+					case movedPermanently:
+					case temporaryRedirect:
+						res.redirect(r.headers["Location"]);
+						break;
+
+					default:
+						throw new Exception("Unhandled error code " ~ httpStatusText(status));
+				}
+			}
+
+			logInfo(res.toString());			
+		}
+	);
 }
