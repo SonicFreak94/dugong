dd63fbde93c6776058446bfee37a65c6258a5247
Michael Fadely <michaelwfadely@gmail.com>
2017-01-11 06:56:46 -0700

    this works???

--- source/app.d
+++ source/app.d
@@ -1,4 +1,7 @@
+import std.conv;
 import std.stdio;
+import std.string;
+import core.time;
 import vibe.d;
 
 shared static this()
@@ -6,9 +9,38 @@ shared static this()
 	auto settings = new HTTPServerSettings;
 	settings.port = 3128;
 	settings.bindAddresses = ["::1", "127.0.0.1"];
-	listenHTTP(settings, &passThrough);
 
+	auto router = new URLRouter();
+	router.match(HTTPMethod.CONNECT, "*", &handleConnect);
+	router.any("*", &passThrough);
+
+	listenHTTP(settings, router);
 	logInfo("Proxy started on port 3128.");
+}
+
+void handleConnect(HTTPServerRequest req, HTTPServerResponse res)
+{
+	auto host = req.host.split(":");
+
+	res.headers = req.headers;
+	res.statusCode = HTTPStatus.ok;
+	res.writeVoidBody();
+
+	auto tcp = connectTCP(host[0], to!ushort(host[1]));
+	auto con = res.connectProxy();
+
+	scope (exit)
+	{
+		tcp.close();
+		con.close();
+	}
+	
+	runTask({
+		tcp.write(con);
+		tcp.close();
+	});
+
+	con.write(tcp);
 }
 
 void passThrough(HTTPServerRequest req, HTTPServerResponse res)
@@ -42,13 +74,6 @@ void passThrough(HTTPServerRequest req, HTTPServerResponse res)
 
 	try
 	{
-		if (req.method == HTTPMethod.CONNECT)
-		{
-			auto url = URL.parse("http://" ~ req.host ~ "/");
-			logInfo("IDK WHAT TO DO BUT HERE TAKE THIS: %s:%u", url.host, url.port);
-			return;
-		}
-
 		requestHTTP(req.requestURL, toDelegate(&proxyRequest), toDelegate(&proxyResponse));
 	}
 	catch (Exception ex)
