74a54ce54950186af29b04989427115f651921e1
Michael Fadely <michaelwfadely@gmail.com>
2017-01-11 01:17:38 -0700

    cleanup

--- source/app.d
+++ source/app.d
@@ -1,3 +1,4 @@
+import std.stdio;
 import vibe.d;
 
 shared static this()
@@ -12,50 +13,47 @@ shared static this()
 
 void passThrough(HTTPServerRequest req, HTTPServerResponse res)
 {
+	// don't mind me, just putting functions in functions
+
+	void proxyRequest(scope HTTPClientRequest r)
+	{
+		r.headers = req.headers;
+		r.method = req.method;
+		r.bodyWriter.write(req.bodyReader);
+	}
+
+	void proxyResponse(scope HTTPClientResponse r)
+	{
+		auto status = cast(HTTPStatus)r.statusCode;
+
+		with (HTTPStatus) switch (status)
+		{
+			case movedPermanently:
+			case temporaryRedirect:
+				res.redirect(r.headers["Location"]);
+				break;
+
+			default:
+				res.headers = r.headers;
+				res.bodyWriter.write(r.bodyReader);
+				break;
+		}
+	}
+
 	try
 	{
 		if (req.method == HTTPMethod.CONNECT)
 		{
-			logInfo("IDK WHAT TO DO BUT HERE TAKE THIS: " ~ req.toString());
+			auto url = URL.parse("http://" ~ req.host ~ "/");
+			logInfo("IDK WHAT TO DO BUT HERE TAKE THIS: %s:%u", url.host, url.port);
 			return;
 		}
 
-		requestHTTP(req.requestURL,
-			(scope HTTPClientRequest r)
-			{
-				r.headers = req.headers;
-				r.method = req.method;
-				r.bodyWriter.write(req.bodyReader);
-			},
-			(scope HTTPClientResponse r)
-			{
-				auto status = cast(HTTPStatus)r.statusCode;
-
-				if (isSuccessCode(status))
-				{
-					res.headers = r.headers;
-					res.bodyWriter.write(r.bodyReader);
-				}
-				else
-				{
-					with (HTTPStatus) switch (status)
-					{
-						case movedPermanently:
-						case temporaryRedirect:
-							res.redirect(r.headers["Location"]);
-							break;
-
-						default:
-							res.headers = r.headers;
-							res.bodyWriter.write(r.bodyReader);
-							break;
-					}
-				}
-			}
-		);
+		requestHTTP(req.requestURL, toDelegate(&proxyRequest), toDelegate(&proxyResponse));
 	}
 	catch (Exception ex)
 	{
 		res.writeBody(ex.toString());
+		stderr.writeln(ex.msg);
 	}
 }
