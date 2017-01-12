f26f9445f15b6abdc3d24e340bab2838eb07a602
Michael Fadely <michaelwfadely@gmail.com>
2017-01-11 20:10:47 -0700

    working on POST

--- source/app.d
+++ source/app.d
@@ -1,7 +1,6 @@
 import std.conv;
 import std.stdio;
 import std.string;
-import core.time;
 import vibe.d;
 
 shared static this()
@@ -9,10 +8,11 @@ shared static this()
 	auto settings = new HTTPServerSettings;
 	settings.port = 3128;
 	settings.bindAddresses = ["::1", "127.0.0.1"];
+	settings.options = HTTPServerOption.defaults;
 
 	auto router = new URLRouter();
 	router.match(HTTPMethod.CONNECT, "*", &handleConnect);
-	router.any("*", &passThrough);
+	router.any("*", &handleAny);
 
 	listenHTTP(settings, router);
 	logInfo("Proxy started on port 3128.");
@@ -20,6 +20,7 @@ shared static this()
 
 void handleConnect(HTTPServerRequest req, HTTPServerResponse res)
 {
+	stderr.writeln(req.toString());
 	auto host = req.host.split(":");
 
 	res.headers = req.headers;
@@ -43,14 +44,36 @@ void handleConnect(HTTPServerRequest req, HTTPServerResponse res)
 	con.write(tcp);
 }
 
-void passThrough(HTTPServerRequest req, HTTPServerResponse res)
+void handleAny(HTTPServerRequest req, HTTPServerResponse res)
 {
+	stderr.writeln(req.toString());
 	// don't mind me, just putting functions in functions
 
 	void proxyRequest(scope HTTPClientRequest r)
 	{
 		r.headers = req.headers;
-		r.method = req.method;
+		r.method  = req.method;
+
+		if (r.method == HTTPMethod.POST)
+		{
+			stderr.writeln("/!\\ Request form: ");
+			
+			// TODO: std.range.zip
+			string[string] stupid;
+			foreach (f; req.form.toRepresentation())
+			{
+				stderr.writefln("%s: %s", f.key, f.value);
+				stupid[f.key] = f.value;
+			}
+
+			// TODO: actually this
+			stderr.writeln();
+			stderr.writeln("/!\\ Request files: ", req.files);
+			stderr.writeln();
+
+			r.writeFormBody(stupid);
+		}
+
 		r.bodyWriter.write(req.bodyReader);
 	}
 
@@ -66,19 +89,16 @@ void passThrough(HTTPServerRequest req, HTTPServerResponse res)
 				break;
 
 			default:
+				stderr.writeln("Response code: ", to!string(status));
+				res.cookies = r.cookies;
 				res.headers = r.headers;
+				res.statusCode = r.statusCode;
+				res.statusPhrase = r.statusPhrase;
+
 				res.bodyWriter.write(r.bodyReader);
 				break;
 		}
 	}
 
-	try
-	{
-		requestHTTP(req.requestURL, toDelegate(&proxyRequest), toDelegate(&proxyResponse));
-	}
-	catch (Exception ex)
-	{
-		res.writeBody(ex.toString());
-		stderr.writeln(ex.msg);
-	}
+	requestHTTP(req.requestURL, toDelegate(&proxyRequest), toDelegate(&proxyResponse));
 }
